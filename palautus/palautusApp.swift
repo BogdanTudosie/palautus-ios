87b7fe150505445b46acc2a8e1b1f4ea40c362b3
Giuseppe Franco <franco.giuseppe@gmail.com>
2025-02-10 22:54:34 +0200

    added base project

---
+++ palautus/palautusApp.swift
@@ -0,0 +1,17 @@
+//
+//  palautusApp.swift
+//  Palautus.ai
+//
+//  Created by Giuseppe on 10.2.2025.
+//
+
+import SwiftUI
+
+@main
+struct palautusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ palautusTests/palautusTests.swift
@@ -0,0 +1,17 @@
+//
+//  palautusTests.swift
+//  palautusTests
+//
+//  Created by Giuseppe on 10.2.2025.
+//
+
+import Testing
+@testable import palautus
+
+struct palautusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
